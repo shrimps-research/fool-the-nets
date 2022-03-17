40cc71372c8ff2290d5d066c1657d309334f3d39
pavlosZakkas <pavloszk93@gmail.com>
2022-03-17 16:25:02 +0100

    Add PyTorch dataloaders handler
    Given a dataset name, get the PyTorch DataLoader.
    
    Note: This implementation has not been tested yet.

---
+++ src/data/dataloaders.py
@@ -0,0 +1,35 @@
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms as T
+import torch
+import os
+
+from src.data.datasets import IMAGENET200
+from src.settings import DEFAULT_DATASET_PATH
+
+def generate_dataloader(
+  dataset=IMAGENET200,
+  dataset_type='train',
+  batch_size=2,
+  transform=T.ToTensor(),
+  datasets_path=DEFAULT_DATASET_PATH
+):
+  parent_dataset_path = os.path.join(datasets_path, dataset)
+  requested_dataset_path = os.path.join(parent_dataset_path, dataset_type)
+
+  if not os.path.isdir(parent_dataset_path):
+    raise Exception(f'Dataset {dataset} does not exist under {datasets_path} directory')
+  if not os.path.isdir(requested_dataset_path):
+    raise Exception(f'{dataset_type} set does not exist under {datasets_path}/{dataset} directory')
+
+  dataset = datasets.ImageFolder(requested_dataset_path, transform=transform)
+
+  kwargs = {"pin_memory": True, "num_workers": 1} if torch.cuda.is_available() else {}
+
+  return DataLoader(
+    dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    **kwargs
+  )
+
