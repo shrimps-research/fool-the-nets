d0475a43db3be0b85c56999ab80bdb6103a17001
pavlosZakkas <pavloszk93@gmail.com>
2022-04-10 17:37:01 +0200

    Add PGD

---
+++ src/attacks/white_box/pgd.py
@@ -0,0 +1,132 @@
+import argparse
+import numpy as np
+from torchvision import transforms as T
+
+from src.attacks.white_box.transforms.projected_gradient import ProjectedGradientTransform
+from src.data.dataloaders import get_dataset_and_dataloader
+from src.data import visualizer
+from src.data.datasets import IMAGENET100
+from src.models.common import evaluate
+from src.models.perceiver import get_perceiver_io, PERCEIVER_IO_LEARNED_POS_EMBEDDINGS
+from src.models.vit import get_vit, ViT
+
+SUPPORTED_MODEL_NAMES = [
+  PERCEIVER_IO_LEARNED_POS_EMBEDDINGS,
+  ViT
+]
+
+GET_MODEL_FUNCTION_BY_NAME = {
+  PERCEIVER_IO_LEARNED_POS_EMBEDDINGS: get_perceiver_io,
+  ViT: get_vit
+}
+
+def pgd(
+  source_model_name,
+  target_model_name,
+  epsilon,
+  step,
+  iterations,
+  size,
+  batch_size
+):
+
+  source_model = GET_MODEL_FUNCTION_BY_NAME[source_model_name](source_model_name)
+  if source_model_name == target_model_name:
+    target_model = source_model
+  else:
+    target_model = GET_MODEL_FUNCTION_BY_NAME[target_model_name](target_model_name)
+
+  train_set, train_dataloader = get_dataset_and_dataloader(
+    dataset=IMAGENET100,
+    dataset_type='train.X1',
+    max_size=size,
+    batch_size=batch_size,
+    transform=T.Compose([
+      T.ToTensor(),
+      T.Normalize(0, 1),
+      T.CenterCrop(250),
+      T.Resize(source_model.expected_image_size)
+    ]),
+  )
+
+  visualizer.show_image(train_dataloader)
+  # visualizer.show_batch(train_dataloader)
+
+
+  results = evaluate(
+    target_model.model,
+    train_dataloader,
+    target_model.feature_extractor,
+  )
+  print("Accuracy on train set:", results.accuracy)
+  print("Average confidence on target class:", np.mean(results.confidences, axis=0))
+
+  adversarial_train_set, adversarial_train_dataloader = get_dataset_and_dataloader(
+    dataset=IMAGENET100,
+    dataset_type='train.X1',
+    max_size=size,
+    batch_size=batch_size,
+    transform=T.Compose([
+      T.ToTensor(),
+      T.Normalize(0, 1),
+      T.CenterCrop(250),
+      T.Resize(source_model.expected_image_size),
+      ProjectedGradientTransform(
+        source_model.model,
+        epsilon=epsilon,
+        step_size=step,
+        iterations=iterations
+      )
+    ]),
+  )
+
+  visualizer.show_image(adversarial_train_dataloader)
+  # visualizer.show_batch(adversarial_train_dataloader)
+
+  results = evaluate(
+    target_model.model,
+    adversarial_train_dataloader,
+    target_model.feature_extractor,
+  )
+  print("Accuracy on train set attacked by PGD method:", results.accuracy)
+  print("Average confidence on target class after PGD attack:", np.mean(results.confidences, axis=0))
+
+
+def parsed_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    '--model',
+    help='Model to attack',
+    default=ViT,
+    choices=SUPPORTED_MODEL_NAMES,
+    type=str
+  )
+  parser.add_argument(
+    '--epsilon', help='Epsilon parameter of PGD', default=0.03, type=int
+  )
+  parser.add_argument(
+    '--step', help='Epsilon parameter of PGD', default=0.005, type=int
+  )
+  parser.add_argument(
+    '--iterations', help='Epsilon parameter of PGD', default=10, type=int
+  )
+  parser.add_argument(
+    '--size', help='Number of images to attack', default=10, type=int
+  )
+  parser.add_argument(
+    '--batch', help='Batch size', default=5, type=int
+  )
+  return parser.parse_args()
+
+
+if __name__ == "__main__":
+  args = parsed_args()
+  pgd(
+    args.model,
+    args.model,
+    args.epsilon,
+    args.step,
+    args.iterations,
+    args.size,
+    args.batch
+  )
---
+++ src/attacks/white_box/transforms/projected_gradient.py
@@ -0,0 +1,52 @@
+import torch
+import numpy as np
+from cleverhans.torch.attacks.projected_gradient_descent import projected_gradient_descent
+
+class ProjectedGradientTransform(torch.nn.Module):
+
+  def __init__(
+    self,
+    model,
+    target=None,
+    epsilon=0.03,
+    step_size=0.005,
+    iterations=10,
+    norm=np.inf
+  ):
+    super().__init__()
+    self.model = model
+    self.epsilon = epsilon
+    self.step_size = step_size
+    self.iterations = iterations
+    self.norm = norm
+    self.target = target
+
+  def forward(self, img):
+    if self.target is None:
+      image_with_noise = projected_gradient_descent(
+        model_fn=self.model,
+        x=img.unsqueeze(0),
+        eps=self.epsilon,
+        eps_iter=self.step_size,
+        nb_iter=self.iterations,
+        norm=self.norm,
+      )
+    else:
+      image_with_noise = projected_gradient_descent(
+        model_fn=self.model,
+        x=img.unsqueeze(0),
+        eps=self.epsilon,
+        eps_iter=self.step_size,
+        nb_iter=self.iterations,
+        norm=self.norm,
+        y=self.target,
+        targeted=True,
+      )
+
+    return image_with_noise.squeeze()
+
+
+  def __repr__(self) -> str:
+    return f"{self.__class__.__name__}(epsilon={self.epsilon})"
+
+
