3251c35d04264ecf39bd1f7737fd524a1df08566
pavlosZakkas <pavloszk93@gmail.com>
2022-06-25 18:32:06 +0200

    Adapt run_experiments to run on multiple target models

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -1,35 +1,26 @@
 from src.attacks.white_box.transforms.fast_gradient import FastGradientTransform
 from src.attacks.white_box.transforms.projected_gradient import ProjectedGradientTransform
 from src.attacks.white_box.transforms.carlini_wagner import CarliniWagnerL2
+from src.attacks.black_box.transforms.random_noise import GaussianNoiseTransform
 from src.data.datasets import IMAGENET100
 from src.attacks.black_box.fgsm_transfer import adversarial_attack
 import concurrent.futures
-import itertools
 import time
 import multiprocessing
 
 DATASET = IMAGENET100
 DATASET_DIR = 'train.X1'
-SAMPLES_SIZE = 100
+SAMPLES_SIZE = 5000
 BATCH_SIZE = 2
 EPSILON = 0.025
 
-models = [
-  'vit',
-  'perceiver-io',
-  'swin',
-  'vgg',
-  'xception',
+run_configs = [
+  ('vit', ['xception', 'swin', 'vgg', 'vit', 'perceiver-io']),
+  ('xception', ['xception', 'swin', 'vgg', 'vit', 'perceiver-io']),
+  ('vgg', ['xception', 'swin', 'vgg', 'vit', 'perceiver-io']),
+  ('swin', ['xception', 'swin', 'vgg', 'vit', 'perceiver-io']),
+  ('perceiver-io', ['xception', 'swin', 'vgg', 'vit', 'perceiver-io']),
 ]
-
-run_configs = list(itertools.permutations(models, 2))
-# run_configs = [
-#   ('vit', 'vit'),
-#   ('perceiver-io', 'perceiver-io'),
-#   ('swin', 'swin'),
-#   ('vgg', 'vgg'),
-#   ('xception', 'xception'),
-# ]
 
 def attack(source, target):
   adversarial_attack(
@@ -37,9 +28,9 @@ def attack(source, target):
     target,
     SAMPLES_SIZE,
     BATCH_SIZE,
-    methodToRun=FastGradientTransform,
-    # kwargs={'epsilon': EPSILON, 'step_size': EPSILON/10, 'iterations': 50},
-    kwargs={'epsilon': EPSILON},
+    methodToRun=ProjectedGradientTransform,
+    kwargs={'epsilon': EPSILON, 'step_size': EPSILON/10, 'iterations': 50},
+    # kwargs={'epsilon': 0.05},
   )
 
 
